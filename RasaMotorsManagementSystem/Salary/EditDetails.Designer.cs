e70b8fdfbed961f50048abcedc5aac6dde0a31cb
Oshiniii <oshianoliegmail.com>
2019-05-10 20:58:30 +0530

    Salary By Anolie.

---
+++ RasaMotorsManagementSystem/Salary/EditDetails.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace RasaMotorsManagementSystem.Salary
+{
+    partial class EditDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labetf = new System.Windows.Forms.Label();
+            this.labot = new System.Windows.Forms.Label();
+            this.labbasic = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.back = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.comboStates = new System.Windows.Forms.ComboBox();
+            this.lblStates = new System.Windows.Forms.Label();
+            this.txtEpf = new System.Windows.Forms.TextBox();
+            this.LBPayments = new System.Windows.Forms.Label();
+            this.lblEtf = new System.Windows.Forms.Label();
+            this.lblBasic = new System.Windows.Forms.Label();
+            this.txtBasic = new System.Windows.Forms.TextBox();
+            this.TxtPayments = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labetf
+            // 
+            this.labetf.AutoSize = true;
+            this.labetf.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labetf.Location = new System.Drawing.Point(525, 321);
+            this.labetf.Name = "labetf";
+            this.labetf.Size = new System.Drawing.Size(85, 13);
+            this.labetf.TabIndex = 93;
+            this.labetf.Text = "INVALID INPUT";
+            this.labetf.Visible = false;
+            // 
+            // labot
+            // 
+            this.labot.AutoSize = true;
+            this.labot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labot.Location = new System.Drawing.Point(525, 271);
+            this.labot.Name = "labot";
+            this.labot.Size = new System.Drawing.Size(85, 13);
+            this.labot.TabIndex = 92;
+            this.labot.Text = "INVALID INPUT";
+            this.labot.Visible = false;
+            // 
+            // labbasic
+            // 
+            this.labbasic.AutoSize = true;
+            this.labbasic.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labbasic.Location = new System.Drawing.Point(525, 220);
+            this.labbasic.Name = "labbasic";
+            this.labbasic.Size = new System.Drawing.Size(85, 13);
+            this.labbasic.TabIndex = 91;
+            this.labbasic.Text = "INVALID INPUT";
+            this.labbasic.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(346, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 39);
+            this.label1.TabIndex = 86;
+            this.label1.Text = "Edit Details";
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.Color.Maroon;
+            this.back.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.back.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back.ForeColor = System.Drawing.Color.White;
+            this.back.Location = new System.Drawing.Point(446, 379);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(90, 31);
+            this.back.TabIndex = 90;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = false;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Maroon;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(310, 379);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 31);
+            this.btnAdd.TabIndex = 89;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // comboStates
+            // 
+            this.comboStates.FormattingEnabled = true;
+            this.comboStates.Location = new System.Drawing.Point(347, 156);
+            this.comboStates.Name = "comboStates";
+            this.comboStates.Size = new System.Drawing.Size(172, 21);
+            this.comboStates.TabIndex = 83;
+            this.comboStates.SelectedIndexChanged += new System.EventHandler(this.comboStates_SelectedIndexChanged);
+            // 
+            // lblStates
+            // 
+            this.lblStates.AutoSize = true;
+            this.lblStates.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold);
+            this.lblStates.ForeColor = System.Drawing.Color.White;
+            this.lblStates.Location = new System.Drawing.Point(219, 153);
+            this.lblStates.Name = "lblStates";
+            this.lblStates.Size = new System.Drawing.Size(84, 22);
+            this.lblStates.TabIndex = 82;
+            this.lblStates.Text = "Job States";
+            // 
+            // txtEpf
+            // 
+            this.txtEpf.Location = new System.Drawing.Point(347, 314);
+            this.txtEpf.Name = "txtEpf";
+            this.txtEpf.Size = new System.Drawing.Size(172, 20);
+            this.txtEpf.TabIndex = 87;
+            this.txtEpf.TextChanged += new System.EventHandler(this.txtEpf_TextChanged);
+            // 
+            // LBPayments
+            // 
+            this.LBPayments.AutoSize = true;
+            this.LBPayments.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold);
+            this.LBPayments.ForeColor = System.Drawing.Color.White;
+            this.LBPayments.Location = new System.Drawing.Point(219, 258);
+            this.LBPayments.Name = "LBPayments";
+            this.LBPayments.Size = new System.Drawing.Size(110, 22);
+            this.LBPayments.TabIndex = 81;
+            this.LBPayments.Text = "OT Payments";
+            // 
+            // lblEtf
+            // 
+            this.lblEtf.AutoSize = true;
+            this.lblEtf.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEtf.ForeColor = System.Drawing.Color.White;
+            this.lblEtf.Location = new System.Drawing.Point(219, 311);
+            this.lblEtf.Name = "lblEtf";
+            this.lblEtf.Size = new System.Drawing.Size(58, 22);
+            this.lblEtf.TabIndex = 80;
+            this.lblEtf.Text = "Etf&Epf";
+            // 
+            // lblBasic
+            // 
+            this.lblBasic.AutoSize = true;
+            this.lblBasic.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold);
+            this.lblBasic.ForeColor = System.Drawing.Color.White;
+            this.lblBasic.Location = new System.Drawing.Point(219, 208);
+            this.lblBasic.Name = "lblBasic";
+            this.lblBasic.Size = new System.Drawing.Size(99, 22);
+            this.lblBasic.TabIndex = 79;
+            this.lblBasic.Text = "Basic Salary";
+            // 
+            // txtBasic
+            // 
+            this.txtBasic.Location = new System.Drawing.Point(347, 211);
+            this.txtBasic.Multiline = true;
+            this.txtBasic.Name = "txtBasic";
+            this.txtBasic.Size = new System.Drawing.Size(172, 22);
+            this.txtBasic.TabIndex = 84;
+            this.txtBasic.TextChanged += new System.EventHandler(this.txtBasic_TextChanged);
+            // 
+            // TxtPayments
+            // 
+            this.TxtPayments.Location = new System.Drawing.Point(347, 264);
+            this.TxtPayments.Name = "TxtPayments";
+            this.TxtPayments.Size = new System.Drawing.Size(172, 20);
+            this.TxtPayments.TabIndex = 85;
+            this.TxtPayments.TextChanged += new System.EventHandler(this.TxtPayments_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RasaMotorsManagementSystem.Properties.Resources.Rasa_Motors_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(191, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 88;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EditDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labetf);
+            this.Controls.Add(this.labot);
+            this.Controls.Add(this.labbasic);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.comboStates);
+            this.Controls.Add(this.lblStates);
+            this.Controls.Add(this.txtEpf);
+            this.Controls.Add(this.LBPayments);
+            this.Controls.Add(this.lblEtf);
+            this.Controls.Add(this.lblBasic);
+            this.Controls.Add(this.txtBasic);
+            this.Controls.Add(this.TxtPayments);
+            this.Name = "EditDetails";
+            this.Text = "EditDetails";
+            this.Load += new System.EventHandler(this.EditDetails_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labetf;
+        private System.Windows.Forms.Label labot;
+        private System.Windows.Forms.Label labbasic;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ComboBox comboStates;
+        private System.Windows.Forms.Label lblStates;
+        private System.Windows.Forms.TextBox txtEpf;
+        private System.Windows.Forms.Label LBPayments;
+        private System.Windows.Forms.Label lblEtf;
+        private System.Windows.Forms.Label lblBasic;
+        private System.Windows.Forms.TextBox txtBasic;
+        private System.Windows.Forms.TextBox TxtPayments;
+    }
+}
